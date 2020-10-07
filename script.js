bf8f4912c446e50f06e3d012e5cf1d8e0cd6cf2e
Vivek-Vishwakarma <vivek.vishwakarma10022002@gmail.com>
2020-10-07 17:14:42 +0530

    Added my files

---
+++ script.js
@@ -0,0 +1,10 @@
+function fun() {
+    var x = document.querySelector(".innerElement");
+    x.classList.toggle("after");
+    var y = document.querySelector(".line2");
+    y.classList.toggle("lines");
+    //var z = document.querySelector(".line2");
+    //z.classList.toggle("lines2");
+    //var a = document.querySelector(".line3");
+   // a.classList.toggle("lines3");
+}
